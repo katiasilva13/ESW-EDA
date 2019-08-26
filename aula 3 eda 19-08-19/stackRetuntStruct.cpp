681fb7dc0f7d3d9158e8258fc7423dc3a23b85fe
Katia <ktiax13@gmail.com>
2019-08-25 23:50:13 -0300

    Aula 19-08-19 - v1.1

--- aula 3 eda 19-08-19/stackRetuntStruct.cpp
+++ aula 3 eda 19-08-19/stackRetuntStruct.cpp
@@ -1,29 +1,29 @@
-/*
-	Return a `struct` from a function in C
-	url: https://stackoverflow.com/questions/9653072/return-a-struct-from-a-function-in-c
-
-*/
-#include <stdio.h>
-
-struct MyObj{
-    double x, y;
-};
-
-struct MyObj foo(){
-    struct MyObj a;
-
-    a.x = 10;
-    a.y = 10;
-
-    return a;
-}        
-
-int main () {
-
-    struct MyObj a;
-
-    a = foo();    // This DOES work
-    struct MyObj b = a; // This does not work
-
-    return 0;
-}  
+///*
+//	Return a `struct` from a function in C
+//	url: https://stackoverflow.com/questions/9653072/return-a-struct-from-a-function-in-c
+//
+//*/
+//#include <stdio.h>
+//
+//struct MyObj{
+//    double x, y;
+//};
+//
+//struct MyObj foo(){
+//    struct MyObj a;
+//
+//    a.x = 10;
+//    a.y = 10;
+//
+//    return a;
+//}        
+//
+//int main () {
+//
+//    struct MyObj a;
+//
+//    a = foo();    // This DOES work
+//    struct MyObj b = a; // This does not work
+//
+//    return 0;
+//}  
