a349dbff9b66830d578b6f4f3c2c6e2e51f7d060
Katia <ktiax13@gmail.com>
2019-08-26 16:59:08 -0300

    Aula 19-08-19 - v1.4

--- aula 3 eda 19-08-19/ex03v3.cpp
+++ aula 3 eda 19-08-19/ex03v3.cpp
@@ -11,7 +11,6 @@
 	struct realtype{
 		int esquerda;
 		int direita;  
-
 	};
 	
 	void imprimeDireita(struct realtype a){
@@ -30,9 +29,38 @@
 		temp.direita++;
 		return temp;
 	}
+	
+	double retornaReal(struct realtype a){
+		double x, y, real;
+		x = a.esquerda;
+		y = a.direita;
+		y = y/100;
+		if (a.esquerda>=0){
+			real = x + y;			
+		}else{
+			x = abs(x);
+			real = x + y;
+			double temp = real + real;
+			real = real - temp;
+		}
+		return real;
+	}	
 				
-	void imprime(realtype* real){
-		printf("O numero real eh: %d.%d \n", real->esquerda, real->direita);
+	void imprime(struct realtype a){
+		printf("O numero real eh: %f \n", retornaReal(a));
+	}
+	
+	double somar(struct realtype a,struct realtype b, struct realtype c){
+//	double somar(struct realtype a,struct realtype b){
+				
+		double x, y, total;
+		x = retornaReal(a);
+		y = retornaReal(b);
+				total = x + y; 
+		c = criaReal(total);
+	//	imprime(c);	
+	printf("Soma: %f \n", total);
+		return total;
 	}
 
 int main(){
@@ -40,19 +68,22 @@ int main(){
 	double num = 15.17;  
 	struct realtype r1 = criaReal(num);
 	struct realtype r2;
-	
-	r2.direita = 27;
 	r2.esquerda = -3;
-	
-//	imprimeDireita(r1);
-//	imprimeEsquerda(r1); 
+	r2.direita = 27;	
+	struct realtype r3 = {-6, 39};
+	struct realtype r4;	
+		imprime(r4);
+//	imprimeDireita(r3);
+//	imprimeEsquerda(r3); 
+//  imprime(&r4);
+//	printf("Retorna numero real: %f \n",retornaReal(r1));	
 
-//	imprimeDireita(r2);
-//	imprimeEsquerda(r2); 
-  	
-	imprime(&r1);	
-	imprime(&r2);
-    
+//	printf("Soma: %f \n", somar(r1, r2, r4));
+	somar(r1, r2, r4);
+	
+//	r4 = criaReal(somar(r1, r2));	
+	imprime(r4);
+	    
 	return 0;    
 }
 	
