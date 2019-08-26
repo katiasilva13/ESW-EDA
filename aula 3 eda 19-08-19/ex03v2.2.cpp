1af8f63228814b2ac5cbbe978053172e0f059b2c
Katia <ktiax13@gmail.com>
2019-08-26 00:44:17 -0300

    Aula 19-08-19 - v1.2

---
+++ aula 3 eda 19-08-19/ex03v2.2.cpp
@@ -0,0 +1,45 @@
+	// #include <iostream>
+	// using namespace std;
+	#include <stdio.h>
+
+struct realtype{
+ int esquerda;
+  int direita;  
+}r;
+
+struct realtype imprime(struct realtype a){
+//  printf("linha 11\n");
+  return a;
+}
+
+int imprimeRight (struct realtype a){
+ // printf("linha 16\n");
+  return a.direita;
+}
+
+int imprimeLeft (struct realtype a){
+//	printf("linha 21\n");	
+  return a.esquerda;
+}
+
+/*
+int soma(int a, int b){
+    int c = a + b;
+    return c;
+}
+*/
+
+int main(){
+
+//printf("linha 34\n");
+  r.direita = 27;
+  r.esquerda = 3;
+  
+	printf("%d \n", imprime(r));
+	printf("%d\n", imprimeRight(r));
+	printf("%d\n",imprimeLeft(r));
+
+  // printf("%d", soma(2, 2));
+    
+  return 0;
+}
